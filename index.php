07a5fcd1560b1218416672522b4494c90cee5072
Kahfi <mashabulkahfi8@gmail.com>
2019-10-16 22:53:46 +0700

    adding index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Obat</title>
+    </head>
+
+    <link rel="stylesheet" href="style.css">
+
+    <body class="center">
+        <div>
+            <h2>Kamu bisa memasukan penyakit yang terdiagnosa</h2>
+            <form action="kontraindikasi.php" method="POST">
+                <select name="daftar_penyakit">
+                    <option value="alergi rinitis">Alergi Rinitis</option>
+                    <option value="asthma">Asthma</option>
+                    <option value="asthma akut">Asthma Akut</option>
+                    <option value="kanker">Kanker</option>
+                    <option value="kolesterol">Kolesterol</option>
+                    <option value="nyeri">Nyeri</option>
+                    <option value="eczema">Eczema</option>
+                    <option value="eczema mulut">Eczema Mulut</option>
+                    <option value="eczema tenggorokan">Eczema Tenggorokan</option>
+                    <option value="diabetes">Diabetes</option>
+                </select>
+
+                <input type="submit" name="submit" value="Submit">
+
+            </form>
+            
+        </div>
+    </body>
+
+</html>
