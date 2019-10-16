220e5488b0bcc9157bfa583530465807b376aaea
Kahfi <mashabulkahfi8@gmail.com>
2019-10-16 22:28:46 +0700

    first commit

---
+++ kontraindikasi.php
@@ -0,0 +1,127 @@
+<html>
+
+<?php
+    session_start();
+    $penyakit=$_POST['daftar_penyakit'];
+    $_SESSION["penyakit"]= $penyakit;
+    
+    if($penyakit=="alergi rinitis") {
+         ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+
+                <input type="submit" name="submit" value="Submit">
+
+            </form>
+    <?php ; }
+    elseif($penyakit=="asthma" ){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="infeksi jamur sistemik">Infeksi Jamur Sistemik</option>
+                    <option value="sensitif mometasone furoate">Senitif Mometasone Furoate</option>
+                    <option value="sensitif ormoterol fumarate">Sensitif Ormoterol Fumurate</option>
+                    <option value="tidak ada">Tidak ada</option>
+
+                </select>
+
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="asthma akut"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="kanker"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="alergi cisplatin">Alergi Cisplatin</option>
+                    <option value="alergi platinum">Alergi Platinum</option>
+                    <option value="depresi sumsum tulang">Depresi Sumsum Tulang</option>
+                    <option value="sensitif doxorubicin">Sensitif Doxorubicin</option>
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="kolesterol"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="gangguan hati">Gangguan Hati</option>
+                    <option value="ibu hamil">Ibu Hamil</option>
+                    <option value="ibu menyusui">Ibu Menyusui</option>
+                    <option value="penyakit hati aktif">Penyakit Hati Aktif</option>
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="nyeri"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="asma bronkial akut">Asma Bronkial Akut</option>
+                    <option value="depresi pernapasan">Depresi Pernapasan</option>
+                    <option value="penyakit hati">Penyakit Hati</option>
+                    <option value="radang usus">Radang Usus</option>
+                    <option value="sulit bernafas">Sulit Bernafas</option>
+                    <option value="tukak lambung">Tukak Lambung</option>
+                    <option value="umur < 12">Umur < 12</option>
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="eczema"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="sensitif pimecrolimus">Sensitif Pimecrolimus</option>
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="eczema mulut"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="eczema tenggorokan"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ; }
+    elseif($penyakit=="diabetes"){
+        ?>
+        <form action="listobat.php" method="POST">
+                <select name="daftar_kontra">
+                    <option value="asidosis metabolik akut">Asidosis Metabolik Akut</option>
+                    <option value="disfungsi ginjal berat">Disfungsi Ginjal Berat</option>
+                    <option value="MTC">Medullary Thyroid Carcinoma (MTC)</option>
+                    <option value="MEN 2">Multiple Endocrine Neoplasia Syndrome type 2</option>
+                    <option value="sensitif sitagliptin">Sensitif Sitagliptin</option>
+                    <option value="tidak ada">Tidak ada</option>
+                </select>
+                <input type="submit" name="submit" value="Submit">
+        </form>
+    <?php ;}
+?>
+</html>
---
+++ listobat.php
@@ -0,0 +1,144 @@
+<html>
+
+<?php
+    session_start();
+    $gejala=$_SESSION["penyakit"];
+    $kontra=$_POST['daftar_kontra'];
+    echo "List obat yang bisa diberikan :";
+    ?> <br>
+    <?php
+    if(($gejala=="alergi rinitis") and ($kontra=="tidak ada")) {
+        echo "1. Cetrizine";
+    }
+    elseif (($gejala=="asthma") and ($kontra=="tidak ada")){
+        echo "1. Singulair" ;?> <br><?php
+        echo "2. Montelukast";?> <br><?php
+        echo "3. Dulera";?> <br><?php
+        echo "4. Prednisone";?> <br><?php
+    }
+    elseif (($gejala=="asthma") and ($kontra=="infeksi jamur sistemik")){
+        echo "1. Singulair" ;?> <br><?php
+        echo "2. Montelukast";?> <br><?php
+        echo "3. Dulera";?> <br><?php
+    }
+    elseif (($gejala=="asthma") and ($kontra=="sensitif mometasone furoate")){
+        echo "1. Singulair" ;?> <br><?php
+        echo "2. Montelukast";?> <br><?php
+        echo "3. Prednisone";?> <br><?php
+    }
+    elseif (($gejala=="asthma") and ($kontra=="sensitif ormoterol furmarate")){
+        echo "1. Singulair" ;?> <br><?php
+        echo "2. Montelukast";?> <br><?php
+        echo "3. Prednisone";?> <br><?php
+    }
+    elseif (($gejala=="asthma akut") and ($kontra=="tidak ada")){
+        echo "1. Singulair" ;?> <br><?php
+        echo "2. Montelukast";?> <br><?php
+        echo "3. Prednisone";?> <br><?php
+    }
+    elseif (($gejala=="kanker") and ($kontra=="tidak ada")){
+        echo "1. Carboplatin" ;?> <br><?php
+        echo "2. Adriamycin";?> <br><?php
+    }
+    elseif (($gejala=="kanker") and ($kontra=="sensitif doxorubicin")){
+        echo "1. Carboplatin" ;?> <br><?php
+    }
+    elseif (($gejala=="kanker") and ($kontra=="alergi cisplatin")){
+        echo "1. Adriamycin" ;?> <br><?php
+    }
+    elseif (($gejala=="kanker") and ($kontra=="alergi platinum")){
+        echo "1. Adriamycin" ;?> <br><?php
+    }
+    elseif (($gejala=="kanker") and ($kontra=="depresi sumsum tulang")){
+        echo "1. Adriamycin" ;?> <br><?php
+    }
+    elseif (($gejala=="kolesterol") and ($kontra=="tidak ada")){
+        echo "1. Simvastatin" ;?> <br><?php
+        echo "2. Atorvastatin";?> <br><?php
+    }
+    elseif (($gejala=="kolesterol") and ($kontra=="ibu hamil")){
+        echo "1. Simvastatin" ;?> <br><?php
+    }
+    elseif (($gejala=="kolesterol") and ($kontra=="ibu menyusui")){
+        echo "1. Simvastatin" ;?> <br><?php
+    }
+    elseif (($gejala=="kolesterol") and ($kontra=="penyakit hati aktif")){
+        echo "1. Simvastatin" ;?> <br><?php
+    }
+    elseif (($gejala=="kolesterol") and ($kontra=="gangguan hati")){
+        echo "1. Atorvastatin" ;?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="tidak ada")){
+        echo "1. Tramadol" ;?> <br><?php
+        echo "2. Ibuprofen";?> <br><?php
+        echo "3. Acetaminophen";?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="asma bronkial akut")){
+        echo "1. Acetaminophen" ;?> <br><?php
+        echo "2. Ibuprofen";?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="depresi penapasan")){
+        echo "1. Acetaminophen" ;?> <br><?php
+        echo "2. Ibuprofen";?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="penyakit hati")){
+        echo "1. Acetaminophen" ;?> <br><?php
+        echo "2. Tramadol";?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="radang usus")){
+        echo "1. Acetaminophen" ;?> <br><?php
+        echo "2. Tramadol";?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="sulit bernafas")){
+        echo "1. Ibuprofen" ;?> <br><?php
+        echo "2. Tramadol";?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="tukak lambung")){
+        echo "1. Acetaminophen" ;?> <br><?php
+        echo "2. Tramadol";?> <br><?php
+    }
+    elseif (($gejala=="nyeri") and ($kontra=="umur < 12")){
+        echo "1. Acetaminophen" ;?> <br><?php
+        echo "2. Ibuprofen";?> <br><?php
+    }
+    elseif (($gejala=="eczema") and ($kontra=="tidak ada")){
+        echo "1. Memetazone" ;?> <br><?php
+        echo "2. Elidel";?> <br><?php
+    }
+    elseif (($gejala=="eczema") and ($kontra=="sensitif pimecrolimus")){
+        echo "1. Memetazone" ;?> <br><?php
+    }
+    elseif (($gejala=="eczema mulut") and ($kontra=="tidak ada")){
+        echo "1. Triamcinolone" ;?> <br><?php
+    }
+    elseif (($gejala=="eczema tenggorokan") and ($kontra=="tidak ada")){
+        echo "1. Triamcinolone" ;?> <br><?php
+    }
+    elseif (($gejala=="diabetes") and ($kontra=="tidak ada")){
+        echo "1. Metformin" ;?> <br><?php
+        echo "2. Januvia";?> <br><?php
+        echo "3. Victoza";?> <br><?php
+    }
+    elseif (($gejala=="diabetes") and ($kontra=="asidosis metabolik akut")){
+        echo "1. Victoza" ;?> <br><?php
+        echo "2. Januvia";?> <br><?php
+    }
+    elseif (($gejala=="diabetes") and ($kontra=="disfungsi ginjal berat")){
+        echo "1. Victoza" ;?> <br><?php
+        echo "2. Januvia";?> <br><?php
+    }
+    elseif (($gejala=="diabetes") and ($kontra=="MTC")){
+        echo "1. Metformin" ;?> <br><?php
+        echo "2. Januvia";?> <br><?php
+    }
+    elseif (($gejala=="diabetes") and ($kontra=="MEN 2")){
+        echo "1. Metformin" ;?> <br><?php
+        echo "2. Januvia";?> <br><?php
+    }
+    elseif (($gejala=="diabetes") and ($kontra=="sensitif sitagliptin")){
+        echo "1. Metformin" ;?> <br><?php
+        echo "2. Victoza";?> <br><?php
+    }
+
+?>
+</html>
