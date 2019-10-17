9669036b0895efbced52355187ad1b8a141ff741
Mira Risty Masyita <44823779+miraristym@users.noreply.github.com>
2019-10-17 10:33:23 +0700

    remove div

--- index.php
+++ index.php
@@ -19,27 +19,24 @@
             <h1>Rekomendasi Obat</h1>
         </div>
         <main>
-            <div id="pilihan">
-                <h2>Kamu bisa memasukan penyakit yang terdiagnosa</h2>
-                <form action="kontraindikasi.php" method="POST">
-                    <select name="daftar_penyakit">
-                        <option value="alergi rinitis">Alergi Rinitis</option>
-                        <option value="asthma">Asthma</option>
-                        <option value="asthma akut">Asthma Akut</option>
-                        <option value="kanker">Kanker</option>
-                        <option value="kolesterol">Kolesterol</option>
-                        <option value="nyeri">Nyeri</option>
-                        <option value="eczema">Eczema</option>
-                        <option value="eczema mulut">Eczema Mulut</option>
-                        <option value="eczema tenggorokan">Eczema Tenggorokan</option>
-                        <option value="diabetes">Diabetes</option>
-                    </select>
+        <h2>Kamu bisa memasukan penyakit yang terdiagnosa</h2>
+            <form action="kontraindikasi.php" method="POST">
+               <select name="daftar_penyakit">
+                    <option value="alergi rinitis">Alergi Rinitis</option>
+                    <option value="asthma">Asthma</option>
+                    <option value="asthma akut">Asthma Akut</option>
+                    <option value="kanker">Kanker</option>
+                    <option value="kolesterol">Kolesterol</option>
+                    <option value="nyeri">Nyeri</option>
+                    <option value="eczema">Eczema</option>
+                    <option value="eczema mulut">Eczema Mulut</option>
+                    <option value="eczema tenggorokan">Eczema Tenggorokan</option>
+                    <option value="diabetes">Diabetes</option>
+                </select>
 
-                    <input type="submit" name="submit" value="Submit">
+                <input type="submit" name="submit" value="Submit">
 
-                </form>
-                
-            </div>
+            </form>
         </main>
     </body>
 
